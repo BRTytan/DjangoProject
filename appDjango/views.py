6e7ba5cb196514af0da582e4ab220fc6076af8ff
unknown <fe-santos00@hotmail.com>
2019-11-14 22:03:38 -0300

    upanddo django

--- appDjango/views.py
+++ appDjango/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def mostrar_index(request):
+    return render(request, "index.html")
+def mostrar_about(request):
+    return render(request, "about.html")
--- projectDjango/urls.py
+++ projectDjango/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from appDjango import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.mostrar_index),
+    path('about/', views.mostrar_about)
 ]
